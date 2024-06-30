da0a85a529fe8efdf1aed29d200a929b4f5a947a
Marcelinus Apakhade <maarrxxy@gmail.com>
2024-06-30 10:10:33 +0100

    Update settings.py
    reverted to SQLITE

--- Trader/settings.py
+++ Trader/settings.py
@@ -85,9 +85,10 @@ import dj_database_url
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
